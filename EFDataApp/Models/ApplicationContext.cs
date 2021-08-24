5ba683b0afeef902012a31e44cc3246f48ffd7bc
IrinaButyugova <irinabutyugova2403@gmail.com>
2021-08-24 14:00:47 +0300

    EF. Sorting

--- EFDataApp/Models/ApplicationContext.cs
+++ EFDataApp/Models/ApplicationContext.cs
@@ -5,6 +5,7 @@ namespace EFDataApp.Models
     public class ApplicationContext : DbContext
     {
         public DbSet<User> Users { get; set; }
+        public DbSet<Company> Companies { get; set; }
 
         public ApplicationContext(DbContextOptions<ApplicationContext> options)
             : base(options)
