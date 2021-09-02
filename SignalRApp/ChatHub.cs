9cd4788cf72c854d53a7a00ac14c56cbff65d8d2
IrinaButyugova <irinabutyugova2403@gmail.com>
2021-09-02 16:52:04 +0300

    Hub context, connected, disconnected

--- SignalRApp/ChatHub.cs
+++ SignalRApp/ChatHub.cs
@@ -1,13 +1,39 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 
 namespace SignalRApp
 {
     public class ChatHub : Hub
     {
-        public async Task Send(string message, string userName)
+        public async Task Send(string message)
         {
-            await this.Clients.All.SendAsync("Send", message, userName);
+            await this.Clients.All.SendAsync("Send", message, Context.ConnectionId);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("Notify", $"{Context.ConnectionId} вошел в чат");
+
+            var context = Context.GetHttpContext();
+            if (context.Request.Cookies.ContainsKey("name"))
+            {
+                string userName;
+                if (context.Request.Cookies.TryGetValue("name", out userName))
+                {
+                    Debug.WriteLine($"name = {userName}");
+                }
+            }
+            Debug.WriteLine($"UserAgent = {context.Request.Headers["User-Agent"]}");
+            Debug.WriteLine($"RemoteIpAddress = {context.Connection.RemoteIpAddress}");
+
+            await base.OnConnectedAsync();
+        }
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await Clients.All.SendAsync("Notify", $"{Context.ConnectionId} покинул в чат");
+            await base.OnDisconnectedAsync(exception);
         }
     }
 }
