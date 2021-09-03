f205dbfb1208f15ebb0a850a5de45c4a6f0de967
IrinaButyugova <irinabutyugova2403@gmail.com>
2021-09-03 14:11:01 +0300

    Work with Dapper.

---
+++ DapperMvcApp/Controllers/HomeController.cs
@@ -0,0 +1,91 @@
+﻿using DapperMvcApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DapperMvcApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        IUserRepository _repo;
+
+        public HomeController(ILogger<HomeController> logger, IUserRepository r)
+        {
+            _logger = logger;
+            _repo = r;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_repo.GetUsers());
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public ActionResult Details(int id)
+        {
+            User user = _repo.Get(id);
+            if (user != null)
+                return View(user);
+            return NotFound();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(User user)
+        {
+            _repo.Create(user);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            User user = _repo.Get(id);
+            if (user != null)
+                return View(user);
+            return NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(User user)
+        {
+            _repo.Update(user);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        [ActionName("Delete")]
+        public ActionResult ConfirmDelete(int id)
+        {
+            User user = _repo.Get(id);
+            if (user != null)
+                return View(user);
+            return NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            _repo.Delete(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
