4440ca6b954d4904a795ac33cc4e37ead305def1
IrinaButyugova <irinabutyugova2403@gmail.com>
2021-09-03 15:13:01 +0300

    Work with Mongodb

---
+++ MongoApp/Controllers/HomeController.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MongoApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MongoApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ProductService db;
+
+        public HomeController(ILogger<HomeController> logger, ProductService context)
+        {
+            _logger = logger;
+            db = context;
+        }
+
+        public async Task<IActionResult> Index(FilterViewModel filter)
+        {
+            var phones = await db.GetProducts(filter.MinPrice, filter.MaxPrice, filter.Name);
+            var model = new IndexViewModel { Products = phones, Filter = filter };
+            return View(model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Product p)
+        {
+            if (ModelState.IsValid)
+            {
+                await db.Create(p);
+                return RedirectToAction("Index");
+            }
+            return View(p);
+        }
+
+        public async Task<IActionResult> Edit(string id)
+        {
+            Product p = await db.GetProduct(id);
+            if (p == null)
+                return NotFound();
+            return View(p);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Product p)
+        {
+            if (ModelState.IsValid)
+            {
+                await db.Update(p);
+                return RedirectToAction("Index");
+            }
+            return View(p);
+        }
+
+        public async Task<IActionResult> Delete(string id)
+        {
+            await db.Remove(id);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<ActionResult> AttachImage(string id)
+        {
+            Product p = await db.GetProduct(id);
+            if (p == null)
+                return NotFound();
+            return View(p);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AttachImage(string id, IFormFile uploadedFile)
+        {
+            if (uploadedFile != null)
+            {
+                await db.StoreImage(id, uploadedFile.OpenReadStream(), uploadedFile.FileName);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public async Task<ActionResult> GetImage(string id)
+        {
+            var image = await db.GetImage(id);
+            if (image == null)
+            {
+                return NotFound();
+            }
+            return File(image, "image/png");
+        }
+    }
+}
